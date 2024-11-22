d9fa5f2e5d5a89e16f8d147383e028af9de83915
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-11-22 13:38:57 +0500

    feat: add base application exceptions

---
+++ src/application/common/exceptions.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+
+from src.domain.common.exceptions import AppError
+
+
+class ApplicationError(AppError):
+    """Base Application Exception."""
+
+    @property
+    def title(self) -> str:
+        return "An application error occurred"
+
+
+class UnexpectedError(ApplicationError):
+    pass
+
+
+class RepoError(UnexpectedError):
+    pass
+
+
+@dataclass(eq=False)
+class MappingError(ApplicationError):
+    _text: str
+
+    @property
+    def title(self) -> str:
+        return self._text
