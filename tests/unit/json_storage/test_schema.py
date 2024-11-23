c4df18733529b2b0d3f6a9638090301615b1a364
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-11-23 12:31:34 +0500

    tests: add test from Schema.from_json

--- tests/unit/json_storage/test_schema.py
+++ tests/unit/json_storage/test_schema.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.application.book.exceptions import BookAlreadyExists, BookNotFound
+from src.application.common.exceptions import MappingError
 from src.infrastructure.db.json.schema import Schema, BookSchema
 
 
@@ -120,3 +121,52 @@ def test_delete():
         schema.delete(book_id=1)
 
 
+def test_from_json():
+    assert Schema.from_json(None) == Schema()
+
+    with pytest.raises(MappingError, match=r'Invalid type'):
+        Schema.from_json(0)
+
+    with pytest.raises(MappingError, match=r'`last_id` is missing'):
+        Schema.from_json({})
+
+    with pytest.raises(MappingError, match=r'must be an integer'):
+        Schema.from_json(dict(last_id="1"))
+
+    with pytest.raises(MappingError, match=r'`books` must be a list'):
+        Schema.from_json(dict(last_id=1, books={"1": 2}))
+
+    with pytest.raises(MappingError, match=r'object `book` must be a dict'):
+        Schema.from_json(dict(
+            last_id=1,
+            books=[
+                [1, 2, 3]
+            ]
+        ))
+
+    with pytest.raises(MappingError, match=r'Missing field'):
+        Schema.from_json(dict(
+            last_id=1,
+            books=[
+                dict(
+                    id=1,
+                    title="Title"
+                )
+            ]
+        ))
+
+    schema = Schema(
+        last_id=1,
+        books={
+            1: BookSchema(
+                id=1,
+                title="test",
+                author="test",
+                year=1,
+                status=True,
+            )
+        }
+    )
+
+    json_data = schema.to_json()
+    assert Schema.from_json(json_data) == schema
